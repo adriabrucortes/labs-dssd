aaa6c9b0cc17e48687081ebecdb385322460559d
AidarIglesias <aidar.iglesias@gmail.com>
2024-04-07 10:40:59 +0200

    i2c_master_bit_ctrl.v comentat

--- p3_3/rtl/i2c_master_bit_ctrl.v
+++ p3_3/rtl/i2c_master_bit_ctrl.v
@@ -59,13 +59,13 @@ module i2c_master_bit_ctrl (
   // whenever the slave is not ready it can delay the cycle by pulling SCL low
   // delay Scl_oen
   always @(posedge Clk or negedge Rst_n)
-    if(!Rst_n) dScl_oen <= 1'b1;
-    else       dScl_oen <= Scl_oen;
+    if(!Rst_n) dScl_oen <= 1'b1;      // Deactivate delayed SCL line output upon reset.
+    else       dScl_oen <= Scl_oen;   // Keep SCL line output enable as it was in previous cycle
 
   // slave_wait is asserted when master wants to drive SCL high, but the slave pulls it low
   // slave_wait remains asserted until the slave releases SCL
   always @(posedge Clk or negedge Rst_n)
-    if (!Rst_n) slave_wait <= 1'b0;
+    if (!Rst_n) slave_wait <= 1'b0;   // No clock stretching upon reset
     else        slave_wait <= (dScl_oen & ~sSCL) | (slave_wait & ~sSCL);
 
   // master drives SCL high, but another master pulls it low
@@ -73,36 +73,36 @@ module i2c_master_bit_ctrl (
   wire scl_sync = dSCL & ~sSCL & Scl_oen;
 
   // generate clk_en signal
-  wire clk_en = TimerOut;
-  assign TimerStart = !Enable | scl_sync;
-  assign TimerStop = slave_wait;
+  wire clk_en = TimerOut;                   // Core system clock given by the SCL timer output
+  assign TimerStart = !Enable | scl_sync;   // Timer starts whenever it is enabled or a master drives SCL high
+  assign TimerStop = slave_wait;            // Timer stops whenever a slave sends wait states
 
 
   // generate bus status controller
   // capture SDA and SCL
   // reduce metastability risk
   always @(posedge Clk or negedge Rst_n)
-    if(!Rst_n) begin
+    if(!Rst_n) begin  // Variables are set to a known value upon reset
       sSCL <= 1'b1;
       sSDA <= 1'b1;
       dSCL <= 1'b1;
       dSDA <= 1'b1;
-    end else begin
-      sSCL <= Scl_i;
-      sSDA <= Sda_i;
-      dSCL <= sSCL;
-      dSDA <= sSDA;
+    end else begin    // LHS values update on next positive edge of Clk:
+      sSCL <= Scl_i;  // I2C clock line input stored to synchronized I2C clock line input
+      sSDA <= Sda_i;  // I2C data line input stored to synchronized I2C data line input
+      dSCL <= sSCL;   // Value of sSCL of this cycle stored to delayed version of sSCL
+      dSDA <= sSDA;   // Value of sSDA of this cycle stored to delayed version of sSDA
     end
 
   // detect start condition => detect falling edge on SDA while SCL is high
   // detect stop condition => detect rising edge on SDA while SCL is high
   always @(posedge Clk or negedge Rst_n)
-    if(!Rst_n) begin
+    if(!Rst_n) begin  // Start/Stop conditions set to known state upon reset
       sta_condition <= 1'b0;
       sto_condition <= 1'b0;
     end else begin
       sta_condition <= ~sSDA &  dSDA &  sSCL; // SCL NEEDS TO BE HIGH, NOT LOW
-      sto_condition <=  sSDA & ~dSDA &  sSCL;
+      sto_condition <=  sSDA & ~dSDA &  sSCL; // Stop condition met when SDA data line is free (comm. ended)
     end
 
 
@@ -110,6 +110,10 @@ module i2c_master_bit_ctrl (
   always @(posedge Clk or negedge Rst_n)
     if(!Rst_n) I2C_busy <= 1'b0;
     else       I2C_busy <= (sta_condition | I2C_busy) & ~sto_condition;
+    /* I2C bus is busy (I2C_busy = 1b'1) as soon as a start condition is met or it 
+    * kept busy from previous cycle and there no stop condition is met. I2C is free 
+    * as soon as stop condition is met or, neither start start condition is met, nor
+    * the I2C bus is busy at the moment. */
 
 
   // generate arbitration lost signal
@@ -117,12 +121,12 @@ module i2c_master_bit_ctrl (
   // 1) master drives SDA high, but the i2c bus is low
   // 2) stop detected while not requested
   always @(posedge Clk or negedge Rst_n)
-    if(!Rst_n)      cmd_stop <= 1'b0;
-    else if(clk_en) cmd_stop <= Cmd == `I2C_CMD_STOP;
-    else            cmd_stop <= cmd_stop;
+    if(!Rst_n)      cmd_stop <= 1'b0;                   // cmd_stop bit is set to 0 upon reset
+    else if(clk_en) cmd_stop <= Cmd == `I2C_CMD_STOP;   // cmd_stop bit is set to 1 if requested Cmd is Stop Command
+    else            cmd_stop <= cmd_stop;               // if not requested, keep cmd_stop as in previous cycle
 
   always @(posedge Clk or negedge Rst_n)
-    if(!Rst_n) I2C_al <= 1'b0;
+    if(!Rst_n) I2C_al <= 1'b0;  // No arbitration loss upon reset
     else       I2C_al <= (sda_chk & ~sSDA & Sda_oen) | (sto_condition & ~cmd_stop);
 
 
@@ -149,79 +153,83 @@ module i2c_master_bit_ctrl (
              WR_C    = 4'd13,
              WR_D    = 4'd14;
 
+  // FSM logic
   always @(posedge Clk or negedge Rst_n)
-    if(!Rst_n) state <= IDLE;
-    else       state <= next;
+    if(!Rst_n) state <= IDLE;   // Return to IDLE state upon reset
+    else       state <= next;   // Step onto the next state every clock cycle
 
-  always @(*)
+  // Case management
+  always @(*)  
     case(state)
       IDLE : if(clk_en)
-               case (Cmd)
-                 `I2C_CMD_START: next = START_B;
-                 `I2C_CMD_STOP : next = STOP_B;
-                 `I2C_CMD_READ : next = RD_B;
-                 `I2C_CMD_WRITE: next = WR_B;
-                 default       : next = IDLE;
+               case (Cmd) // Check requested command when in IDLE state
+                 `I2C_CMD_START: next = START_B;  // set next state to START_B if requested Cmd is Start
+                 `I2C_CMD_STOP : next = STOP_B;   // set next state to STOP_B if requested Cmd is Stop
+                 `I2C_CMD_READ : next = RD_B;     // set next state to RD_B if requested Cmd is Read
+                 `I2C_CMD_WRITE: next = WR_B;     // set next state to WR_B if requested Cmd is Write
+                 default       : next = IDLE;     // if no Cmd is matched, default to IDLE
                endcase
-              else               next = IDLE;
+              else               next = IDLE;     // Stay in IDLE if SCL timer is not enabled
 
-      START_B: if(clk_en) next = START_C;
-               else       next = START_B;
-      START_C: if(clk_en) next = START_D;
-               else       next = START_C;
-      START_D: if(clk_en) next = START_E;
-               else       next = START_D;
-      START_E: if(clk_en) next = START_F;
-               else       next = START_E;
-      START_F: if(clk_en) next = IDLE;
-               else       next = START_F;
+      // Requested command is Start:
+      START_B: if(clk_en) next = START_C;   // From START_B set next state to START_C
+               else       next = START_B;   // Else, stay in START_B
+      START_C: if(clk_en) next = START_D;   // From START_C set next state to START_D
+               else       next = START_C;   // Else, stay in START_C
+      START_D: if(clk_en) next = START_E;   // From START_D set next state to START_E
+               else       next = START_D;   // Else, stay in START_D
+      START_E: if(clk_en) next = START_F;   // From START_E set next state to START_F
+               else       next = START_E;   // Else, stay in START_E
+      START_F: if(clk_en) next = IDLE;      // From START_F set next state to IDLE
+               else       next = START_F;   // Else, stay in START_F
 
-      STOP_B : if(clk_en) next = STOP_C;
-               else       next = STOP_B;
-      STOP_C : if(clk_en) next = STOP_D;
-               else       next = STOP_C;
-      STOP_D : if(clk_en) next = IDLE;
-               else       next = STOP_D;
+      // Requested command is Stop:
+      STOP_B : if(clk_en) next = STOP_C;    // From STOP_B set next state to STOP_C
+               else       next = STOP_B;    // Else, stay in STOP_B
+      STOP_C : if(clk_en) next = STOP_D;    // From START_C set next state to START_D
+               else       next = STOP_C;    // Else, stay in STOP_C
+      STOP_D : if(clk_en) next = IDLE;      // From START_D set next state to IDLE
+               else       next = STOP_D;    // Else, stay in STOP_D
 
-      RD_B   : if(clk_en) next = RD_C;
-               else       next = RD_B;
-      RD_C   : if(clk_en) next = RD_D;
-               else       next = RD_C;
-      RD_D   : if(clk_en) next = IDLE;
-               else       next = RD_D;
+      RD_B   : if(clk_en) next = RD_C;      // From RD_C set next state to RD_C
+               else       next = RD_B;      // Else, stay in RD_B
+      RD_C   : if(clk_en) next = RD_D;      // From RD_C set next state to RD_D
+               else       next = RD_C;      // Else, stay in RD_C
+      RD_D   : if(clk_en) next = IDLE;      // From RD_D set next state to IDLE
+               else       next = RD_D;      // Else, stay in RD_D
 
-      WR_B   : if(clk_en) next = WR_C;
-               else       next = WR_B;
-      WR_C   : if(clk_en) next = WR_D;
-               else       next = WR_C;
-      WR_D   : if(clk_en) next = IDLE;
-               else       next = WR_D;
-
-      default: next = IDLE;
+      WR_B   : if(clk_en) next = WR_C;      // From WR_C set next state to WR_C
+               else       next = WR_B;      // Else, stay in WR_B
+      WR_C   : if(clk_en) next = WR_D;      // From WR_C set next state to WR_D
+               else       next = WR_C;      // Else, stay in WR_C
+      WR_D   : if(clk_en) next = IDLE;      // From WR_D set next state to IDLE
+               else       next = WR_D;      // Else, stay in WR_D
+      default:            next = IDLE;      // If no state is matched, default to IDLE
     endcase
 
+  // SCL/SDA signals logic
   always @(posedge Clk or negedge Rst_n)
-    if(!Rst_n) begin
+    if(!Rst_n) begin    // Set variables to known state upon reset
       Ack <= 1'b0;
       Scl_oen <= 1'b1;
       Sda_oen <= 1'b1;
       sda_chk <= 1'b0;
-    end else if(I2C_al) begin
-      Ack <= 1'b0;
-      Scl_oen <= 1'b1;
-      Sda_oen <= 1'b1;
-      sda_chk <= 1'b0;
-    end else if(clk_en) begin
-      Ack <= 1'b0;
+    end else if(I2C_al) begin // When arbitration is lost:
+      Ack <= 1'b0;      // No command acknowledge
+      Scl_oen <= 1'b1;  // No SCL line output
+      Sda_oen <= 1'b1;  // No SDA line output
+      sda_chk <= 1'b0;  // No SDA line output check
+    end else if(clk_en) begin // When no arbitration is lost:
+      Ack <= 1'b0;  // Prior to each command, set Ack bit to 0 (command completed)
       case(state)
         IDLE : begin
           sda_chk <= 1'b0;    // don't check SDA output
           case (Cmd)
-            `I2C_CMD_START: begin Scl_oen <= Scl_oen; Sda_oen <= 1'b1; end // START_A
-            `I2C_CMD_STOP : begin Scl_oen <= 1'b0;    Sda_oen <= 1'b0; end // STOP_A
-            `I2C_CMD_READ : begin Scl_oen <= 1'b0;    Sda_oen <= 1'b1; end // RD_A
-            `I2C_CMD_WRITE: begin Scl_oen <= 1'b0;    Sda_oen <= Txd; end  // WR_A
-            default       : begin Scl_oen <= Scl_oen; Sda_oen <= Sda_oen; end // TRUE IDLE
+            `I2C_CMD_START: begin Scl_oen <= Scl_oen; Sda_oen <= 1'b1; end // Start Cmd: SCL output kept as before, SDA output disabled
+            `I2C_CMD_STOP : begin Scl_oen <= 1'b0;    Sda_oen <= 1'b0; end // Stop Cmd:  Both outputs are disabled
+            `I2C_CMD_READ : begin Scl_oen <= 1'b0;    Sda_oen <= 1'b1; end // Read Cmd:  SCL output enabled in order to read, SDA disabled (no change in data while reading)
+            `I2C_CMD_WRITE: begin Scl_oen <= 1'b0;    Sda_oen <= Txd; end  // Write Cmd: SCL output enabled in order to write, SDA output set to Txd (data to send)
+            default       : begin Scl_oen <= Scl_oen; Sda_oen <= Sda_oen; end // Keep outputs as before per default
           endcase
         end
 
